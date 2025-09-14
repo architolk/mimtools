68057fee7d5001ac06e21605a622c2fb207e665f
architolk <msj@xs4all.nl>
2025-09-14 13:06:57 +0200

    Support for high level facttypes, using 'undocumented' feature of relatieklasse

--- fbm-mim2graphml.sh
+++ fbm-mim2graphml.sh
@@ -1,1 +1,1 @@
-java -jar ../rdf2xml/target/rdf2xml.jar example-fbm/mim.ttl example-fbm/mim.graphml mim2graphml.xsl
+java -jar ../rdf2xml/target/rdf2xml.jar example-fbm/mim.ttl example-fbm/mim.graphml mim2graphml.xsl add example-fbm/mim-edited.graphml
