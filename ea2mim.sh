0cdd2bf12a36faf2ce290928f35e5ed174c904a1
architolk <msj@xs4all.nl>
2025-10-29 08:52:55 +0100

    Updated script

--- ea2mim.sh
+++ ea2mim.sh
@@ -1,2 +1,2 @@
-#java -jar ../ea2rdf/target/ea2rdf.jar -e example-uml/example.eap > example-uml/example.ttl
+java -jar ../ea2rdf/target/ea2rdf.jar -ea -e example-uml/example.eap > example-uml/example.ttl
 java -jar ../rdf2rdf/target/rdf2rdf.jar example-uml/example.ttl example-models/example.ttl ea2mim.yaml
